dfc45990eef26d88bfa9dd588428f4327404c74a
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-29 20:18:15 +0100

    Add GlobalStyle.ts with resets and globals

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import { mainTheme } from "./styles/mainTheme";
+import GlobalStyle from "./styles/GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={mainTheme}>
+      <GlobalStyle />
       <div />
     </ThemeProvider>
   </React.StrictMode>,
