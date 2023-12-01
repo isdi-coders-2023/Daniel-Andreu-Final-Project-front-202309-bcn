f60bb93b889ab8991b6157cbf7feb1a430f28fc8
Daniel Andreu <daniel1997ar@gmail.com>
2023-12-01 18:15:23 +0100

    Add renderWithProviders function

---
+++ src/testUtils/renderWithProviders.tsx
@@ -0,0 +1,31 @@
+import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import GlobalStyle from "../styles/GlobalStyle";
+import { shoesReducer } from "../store/features/shoes/shoesSlice";
+import shoesMock from "../mocks/shoesMock";
+import { mainTheme } from "../styles/mainTheme";
+
+const customRender = (children: React.ReactElement) => {
+  const mockStore = configureStore({
+    reducer: {
+      shoesState: shoesReducer,
+    },
+    preloadedState: { shoesState: { shoes: shoesMock } },
+  });
+
+  render(
+    <Provider store={mockStore}>
+      <BrowserRouter>
+        <ThemeProvider theme={mainTheme}>
+          <GlobalStyle />
+          {children}
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>,
+  );
+};
+
+export default customRender;
