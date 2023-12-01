ddec784a2d0ebf8c26a22abbb58c9a03de261389
Daniel Andreu <daniel1997ar@gmail.com>
2023-12-01 11:03:32 +0100

    Add types.ts with ShoeStructure

---
+++ src/store/features/shoes/types.ts
@@ -0,0 +1,17 @@
+interface ShoeStructure {
+  id: string;
+  title: string;
+  image: string;
+  price: number;
+  size: number;
+  brand: string;
+  shoeStatus: string;
+  description: string;
+  isBoxIncluded: boolean;
+  isChangesAccepted: boolean;
+  name: string;
+  phone: string;
+  location: string;
+}
+
+export default ShoeStructure;
