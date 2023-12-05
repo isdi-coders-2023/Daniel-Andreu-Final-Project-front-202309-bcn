79c374b57c6bc852f8b96490e2c603c3e328ff57
Daniel Andreu <daniel1997ar@gmail.com>
2023-12-05 12:35:35 +0100

    Add NotFoundPage.tsx and add missing error managmenet in useShoesApi.ts

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,21 @@
+import NotFoundPageStyled from "./NotFoundPageStyled";
+
+const NotFoundPage = (): React.ReactElement => {
+  return (
+    <NotFoundPageStyled className="not-found">
+      <h1 className="not-found__text">
+        Vaya, parece que no hemos encontrado lo que buscas...
+      </h1>
+      <span className="not-found__404">Error 404</span>
+      <img
+        className="not-found__image"
+        src="images/sadBox.svg"
+        alt="página no encontrada"
+        width="250"
+        height="210"
+      />
+    </NotFoundPageStyled>
+  );
+};
+
+export default NotFoundPage;
