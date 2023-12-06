3f5d4f670bf830253c424f9af289c40108f04e6c
Daniel Andreu <daniel1997ar@gmail.com>
2023-12-06 12:51:14 +0100

    Add deleteShoe method in useShoesApi.ts

--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -2,4 +2,5 @@ import { ShoesStateStructure } from "../store/features/shoes/types";
 
 export interface UseShoesApiStructure {
   getShoes: () => Promise<ShoesStateStructure>;
+  deleteShoe: (shoeId: string) => Promise<void>;
 }
