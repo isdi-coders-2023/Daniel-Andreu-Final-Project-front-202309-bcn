e153b1c63a7d48a716d2f47145e04e805e491759
Daniel Andreu <daniel1997ar@gmail.com>
2023-12-07 10:00:57 +0100

    Add deleteShoeFromApi.test.ts

--- src/hooks/__tests__/getShoesFromApi.test.ts
+++ src/hooks/__tests__/getShoesFromApi.test.ts
@@ -1,11 +1,11 @@
 import { renderHook } from "@testing-library/react";
-import { ShoesStateStructure } from "../store/features/shoes/types";
-import shoesMock from "../mocks/shoesMock";
-import useShoesApi from "./useShoesApi";
-import { providerWrapper } from "../testUtils/testUtils";
+import { ShoesStateStructure } from "../../store/features/shoes/types";
+import shoesMock from "../../mocks/shoesMock";
+import useShoesApi from "../useShoesApi";
+import { providerWrapper } from "../../testUtils/testUtils";
 
-describe("Given a useShoesApi custom hook", () => {
-  describe("When it uses it's method getShoes to fetch from te API", () => {
+describe("Given a useShoesApi hook getShoes method", () => {
+  describe("When it's called and there aren't any errors", () => {
     test("Then it should return the available shoes in the API", async () => {
       const expectedShoes: ShoesStateStructure = { shoes: shoesMock };
 
