4f81a99b89df955e3da95f2ba04c0c80b7293e5c
Daniel Andreu <daniel1997ar@gmail.com>
2023-12-07 13:49:37 +0100

    Add NewShoePage.tsx

---
+++ src/pages/NewShoePage/NewShoePage.tsx
@@ -0,0 +1,11 @@
+const NewShoePage = (): React.ReactElement => {
+  return (
+    <main>
+      <h1 className="main-heading">
+        Añade un calzado y véndelo o cambialo por otro
+      </h1>
+    </main>
+  );
+};
+
+export default NewShoePage;
