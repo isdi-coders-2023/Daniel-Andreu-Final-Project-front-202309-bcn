0e40035fd9fb12919764088337ff7ea88d32f4b6
Daniel Andreu <daniel1997ar@gmail.com>
2023-12-13 10:35:50 +0100

    Add ShoeDetail.test.tsx

--- src/components/ShoeCard/ShoeCard.tsx
+++ src/components/ShoeCard/ShoeCard.tsx
@@ -5,6 +5,7 @@ import { ShoeStructure } from "../../store/features/shoes/types";
 import { useAppDispatch } from "../../store/hooks";
 import Button from "../Button/Button";
 import ShoesCardStyled from "./ShoeCardStyled";
+import { Link } from "react-router-dom";
 
 interface ShoeCardProps {
   shoe: ShoeStructure;
@@ -52,52 +53,52 @@ const ShoeCard = ({
       });
     }
   };
+
   return (
     <ShoesCardStyled className="shoe">
-      <div className="shoe__image-container">
-        <img
-          className="shoe__image"
-          src={image}
-          alt={title}
-          width="420"
-          height="420"
-        />
-      </div>
-      <h2 className="shoe__title">{title}</h2>
-      <div className="shoe__info">
-        <span className="shoe__location">{location}</span>
-        <div className="shoe__status-container">
+      <Link to={`/detalle/${_id}`}>
+        <div className="shoe__image-container">
           <img
-            className="shoe__star-status"
-            src="images/star.svg"
-            alt="icono de estrella"
-            width="20"
-            height="20"
+            className="shoe__image"
+            src={image}
+            alt={title}
+            width="420"
+            height="420"
           />
-          <span className="shoe__status">{shoeStatus}</span>
         </div>
-      </div>
-      <div className="shoe__info">
-        <div className="shoe__price-and-changes">
-          <span className="shoe__price">{price}€</span>
-          <span className="shoe__changes">
-            {isChangesAccepted ? "Acepta cambio" : ""}
-          </span>
+        <h2 className="shoe__title">{title}</h2>
+        <div className="shoe__info">
+          <span className="shoe__location">{location}</span>
+          <div className="shoe__status-container">
+            <img
+              className="shoe__star-status"
+              src="images/star.svg"
+              alt="icono de estrella"
+              width="20"
+              height="20"
+            />
+            <span className="shoe__status">{shoeStatus}</span>
+          </div>
         </div>
-        <span className="shoe__size">Talla {size} EU</span>
-      </div>
+        <div className="shoe__info">
+          <div className="shoe__price-and-changes">
+            <span className="shoe__price">{price}€</span>
+            <span className="shoe__changes">
+              {isChangesAccepted ? "Acepta cambio" : ""}
+            </span>
+          </div>
+          <span className="shoe__size">Talla {size} EU</span>
+        </div>
+      </Link>
       <div className="shoe__buttons-container">
         <Button
           type="button"
-          actionOnClick={() => {}}
           className="button__primary-circular"
           text="Modificar"
         />
         <Button
           type="button"
-          actionOnClick={() => {
-            deleteThisShoe();
-          }}
+          actionOnClick={deleteThisShoe}
           className="button__secondary-circular"
           text="Eliminar"
         />
