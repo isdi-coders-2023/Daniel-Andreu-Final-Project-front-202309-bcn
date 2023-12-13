ed2f934211ee379f6b3fc7f8ac7c0df041a66e91
Daniel Andreu <daniel1997ar@gmail.com>
2023-12-13 13:40:13 +0100

    Add new test in App.test.tsx

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
@@ -6,6 +6,8 @@ import { ThemeProvider } from "styled-components";
 import { store } from "../../store";
 import GlobalStyle from "../../styles/GlobalStyle";
 import { mainTheme } from "../../styles/mainTheme";
+import { renderWithProvidersAndPath } from "../../testUtils/testUtils";
+import { shoesMock } from "../../mocks/shoesMock";
 
 describe("Given an App component", () => {
   describe("When it's rendered", () => {
@@ -28,6 +30,22 @@ describe("Given an App component", () => {
       });
 
       expect(accesibleText).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered and a user navigates to the detail page of 'Nike Blazer Blancas'", () => {
+    test("Then it should show 'Daniel Andreu' in a heading", async () => {
+      const shoe = shoesMock[0];
+      const path = `/detalle/${shoe._id}`;
+      const expectedHeading = shoe.name;
+
+      await renderWithProvidersAndPath(<App />, path);
+
+      const heading = screen.getByRole("heading", { name: expectedHeading });
+
+      await waitFor(() => {
+        expect(heading).toBeInTheDocument();
+      });
     });
   });
 });
