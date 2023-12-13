aef5fc3a6163f95145de9633da1d4dabd4795b44
Daniel Andreu <daniel1997ar@gmail.com>
2023-12-13 18:52:36 +0100

    Add new test in BottomNavigation.test.tsx

--- src/components/BottomNavigation/BottomNavigation.test.tsx
+++ src/components/BottomNavigation/BottomNavigation.test.tsx
@@ -1,6 +1,9 @@
 import { screen } from "@testing-library/react";
 import BottomNavigation from "./BottomNavigation";
-import { renderWithProviders } from "../../testUtils/testUtils";
+import {
+  renderWithProviders,
+  renderWithProvidersAndPath,
+} from "../../testUtils/testUtils";
 
 describe("Given a BottomNavigation component", () => {
   describe("When it's rendered", () => {
@@ -12,6 +15,19 @@ describe("Given a BottomNavigation component", () => {
       const listItemsNumber = screen.getAllByRole("listitem").length;
 
       expect(listItemsNumber).toStrictEqual(expectedListItemsNumber);
+    });
+  });
+
+  describe("When it's rendered in favourites page", () => {
+    test("Then it should show an image with the alt text 'icono de favoritos'", () => {
+      const expectedAltText = "icono de favoritos";
+      const path = "/favoritos";
+
+      renderWithProvidersAndPath(<BottomNavigation />, path);
+
+      const altText = screen.getByAltText(expectedAltText);
+
+      expect(altText).toBeInTheDocument();
     });
   });
 });
