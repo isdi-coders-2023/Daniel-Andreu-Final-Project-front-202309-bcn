8b9b2d258506ed5114f5956fbcaff7d732e95d6a
Daniel Andreu <daniel1997ar@gmail.com>
2023-12-13 18:53:10 +0100

    Add new integration test in App.test.tsx

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -8,6 +8,7 @@ import GlobalStyle from "../../styles/GlobalStyle";
 import { mainTheme } from "../../styles/mainTheme";
 import { renderWithProvidersAndPath } from "../../testUtils/testUtils";
 import { shoesMock } from "../../mocks/shoesMock";
+import userEvent from "@testing-library/user-event";
 
 describe("Given an App component", () => {
   describe("When it's rendered", () => {
@@ -15,14 +16,15 @@ describe("Given an App component", () => {
       const expectedAccesibleText = "ReSteps logo";
 
       render(
-        <ThemeProvider theme={mainTheme}>
-          <Provider store={store}>
+        <Provider store={store}>
+          <ThemeProvider theme={mainTheme}>
             <BrowserRouter>
               <GlobalStyle />
               <App />
             </BrowserRouter>
-          </Provider>
-        </ThemeProvider>,
+          </ThemeProvider>
+          ,
+        </Provider>,
       );
 
       const accesibleText = screen.getByRole("img", {
@@ -46,6 +48,57 @@ describe("Given an App component", () => {
       await waitFor(() => {
         expect(heading).toBeInTheDocument();
       });
+    });
+  });
+
+  describe("When it's rendered on the NewShoePage and a user fills de form and click on the 'Añadir calzado' button", () => {
+    test("Then it should navigate to the HomePage showing a toastify feedback message", async () => {
+      const buttonText = "Añadir Calzado";
+      const expectedFeedbackText = "Calzado añadido correctamente";
+      const path = "/añadir-calzado";
+
+      await renderWithProvidersAndPath(<App />, path);
+
+      const priceInputElement = screen.getByRole("textbox", {
+        name: "Precio €",
+      });
+      const imageUrlInputElement = screen.getByRole("textbox", {
+        name: "URL de la imagen",
+      });
+      const titleInputElement = screen.getByRole("textbox", {
+        name: "Título",
+      });
+      const descriptionInputElement = screen.getByRole("textbox", {
+        name: "Descripción",
+      });
+      const nameInputElement = screen.getByRole("textbox", {
+        name: "Nombre completo",
+      });
+      const phoneInputElement = screen.getByRole("textbox", {
+        name: "Número de teléfono",
+      });
+      const locationInputElement = screen.getByRole("textbox", {
+        name: "Localidad",
+      });
+      const sizeSelectElement = screen.getByDisplayValue("Talla");
+      const brandSelectElement = screen.getByDisplayValue("Marca");
+      const buttonElement = screen.getByRole("button", { name: buttonText });
+
+      await userEvent.type(priceInputElement, "1");
+      await userEvent.type(imageUrlInputElement, "1");
+      await userEvent.type(titleInputElement, "1");
+      await userEvent.type(descriptionInputElement, "1");
+      await userEvent.type(nameInputElement, "1");
+      await userEvent.type(phoneInputElement, "1");
+      await userEvent.type(locationInputElement, "1");
+      await userEvent.selectOptions(sizeSelectElement, "38");
+      await userEvent.selectOptions(brandSelectElement, "Nike");
+
+      await userEvent.click(buttonElement);
+
+      const feedbackMessage = await screen.getAllByText(expectedFeedbackText);
+
+      expect(feedbackMessage[0]).toBeInTheDocument();
     });
   });
 });
